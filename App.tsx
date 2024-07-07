f2b9415b8c53b8182f8f8db47a68fa8caae2ca64
sarwar-asik <sarwarasik@gmail.com>
2024-07-07 13:12:06 +0600

    features(navigator): navigator created

--- App.tsx
+++ App.tsx
@@ -4,6 +4,7 @@ import { StatusBar } from 'expo-status-bar';
 import { StyleSheet, Text, View } from 'react-native';
 import Home from './page/Home/Home';
 import Blog from './page/Blogs/Blog';
+import Logo from './components/Common/Logo';
 
 
 
@@ -12,19 +13,22 @@ const Stack = createNativeStackNavigator();
 export default function App() {
   return (
     <NavigationContainer >
-      <Stack.Navigator initialRouteName='Home'>
-        <Stack.Screen name='Home' component={Home} options={{
-          title: "Home Page",
-          headerStyle: {
-            backgroundColor: '#50B498',
-          },
-          headerTintColor: 'white',
-          headerTitleStyle: {
-            fontWeight: 'bold',
-          },
-          headerTitleAlign: "center"
+      <Stack.Navigator initialRouteName='Home' screenOptions={{
+        headerStyle: {
+          backgroundColor: '#50B498',
+        },
+        headerTintColor: 'white',
+        headerTitleStyle: {
+          fontWeight: 'bold',
+        },
+        headerTitleAlign: "center",
+        // headerTitle: () => <Logo />,
+      }}>
+        <Stack.Screen name='Home' component={Home} options={{ title: "Home" }} />
+        <Stack.Screen name='Blog' component={Blog} options={{
+          title: "Blog",
+          headerRight: () => <Logo />
         }} />
-        <Stack.Screen name='Blog' component={Blog} />
       </Stack.Navigator>
     </NavigationContainer>
   );
