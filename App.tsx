91ba3af0088189024a62ceb48eefd819758d50f8
sarwar-asik <sarwarasik@gmail.com>
2024-07-07 13:39:48 +0600

    features(navigator): Drawer Navigator showed

--- App.tsx
+++ App.tsx
@@ -6,22 +6,26 @@ import Home from './page/Home/Home';
 import Blog from './page/Blogs/Blog';
 import Logo from './components/Common/Logo';
 import { createBottomTabNavigator } from '@react-navigation/bottom-tabs';
+import { createDrawerNavigator } from '@react-navigation/drawer';
+
 import Profile from './page/profile/Profile';
 
 
 
 
 // const Stack = createNativeStackNavigator();
-const Tab = createBottomTabNavigator();
+// const Tab = createBottomTabNavigator();
+
+const Drawer = createDrawerNavigator();
 export default function App() {
   return (
     <NavigationContainer>
-      <Tab.Navigator>
-        <Tab.Screen name='Home' component={Home} />
-        <Tab.Screen name='Blog' component={Blog} />
-        <Tab.Screen name='Profile' component={Profile} />
+      <Drawer.Navigator>
+        <Drawer.Screen name='Home' component={Home} />
+        <Drawer.Screen name='Blog' component={Blog} />
+        <Drawer.Screen name='Profile' component={Profile} />
 
-      </Tab.Navigator>
+      </Drawer.Navigator>
     </NavigationContainer>
   );
 }
